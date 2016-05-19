14a663ad5b7b1263f0c6ba7ec0fa9fbad45f62f2
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-05-19 12:27:51 +0200

    add run compileDesktop in this testing framework

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import subprocess, threading, time, select
 
-stopListenLog = False
+stopListenLog        = False
 stopListenLogStopped = False
 
+# extracted info out of video
+lBlinks = []
+rBlinks = []
+
+# parsing logs
 def initListenLog():
     global proc
     proc = subprocess.Popen(
@@ -15,17 +20,32 @@ def initListenLog():
     )
 
 def listenLog():
-    global stopListenLogStopped
+    global stopListenLogStopped, lBlinks, rBlinks
     poll_obj = select.poll()
     poll_obj.register(proc.stdout, select.POLLIN)
     while True:
         poll_result = poll_obj.poll(0)
         if poll_result:
             output = proc.stdout.readline()
-            print "out:", output
+            if output.startswith("debug_blinks_d4:"):
+                blinkInfo = output.split(" ")
+                start     = float(blinkInfo[blinkInfo.index("start")+1])
+                end       = float(blinkInfo[blinkInfo.index("end")+1])
+                duration  = float(blinkInfo[blinkInfo.index("duration")+1])
+                blinkInfoDict = {"start":start, "end":end, "duration":duration}
+
+                if blinkInfo[1] == "adding_lBlinkChunks":
+                    lBlinks.append(blinkInfoDict)
+                elif blinkInfo[1] == "adding_rBlinkChunks":
+                    rBlinks.append(blinkInfoDict)
+
+                print "adding:", blinkInfo[1], blinkInfo
+            elif output.startswith("exiting"):
+                stopListenLogStopped = True
+                break
+
             continue
 
-        print "I'm here"
         time.sleep(0.1)
         if stopListenLog == True:
             stopListenLogStopped = True
@@ -34,23 +54,38 @@ def listenLog():
 
 def terminateListenLog():
     proc.terminate()
+####
 
-
-
+# configure and run video
+def initRunVideo():
+    global vid
+    vid = subprocess.Popen(
+        ['bash', 'compileDesktop'],
+        cwd     = '/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/optical-flow',
+        stdin   = subprocess.PIPE,
+        stdout  = subprocess.PIPE,
+        bufsize = 0,
+    )
+def terminateRunVideo():
+    vid.terminate()
+###
 
 def main():
     global stopListenLog
     initListenLog()
+    time.sleep(0.5)
+    initRunVideo()
 
     listenLogThread = threading.Thread(target=listenLog, args=[])
     listenLogThread.start()
-    time.sleep(5)
+    listenLogThread.join()
 
     stopListenLog = True
     while stopListenLogStopped != True:
         print "Waiting thread to stop"
         time.sleep(0.1)
     terminateListenLog()
+    terminateRunVideo()
 
 
 
