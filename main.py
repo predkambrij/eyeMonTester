6047059bc139e3a0fb1cb9bc9e858859baf299ed
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-06-11 20:33:39 +0200

    upd read annotations from sk style

--- main.py
+++ main.py
@@ -9,18 +9,93 @@ rBlinks = []
 tCors   = []
 
 #videoName = "o44" # doma
-videoName = "o89" # knjiznica 40s
+#videoName = "o89" # knjiznica 40s
+#videoName = "o90" # premikal glavo, zadej luc
+vidPrefix = "/home/developer/other/posnetki/"
+videoName = "talking.avi"
+videoAnnot = os.path.splitext(videoName)[0]+".tag"
+
 path = "/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/test_runner/"
-def generateTCSV():
-    tsdict = {}
+def buildFndict():
+    fndict = {}
     for d in tCors:
-        tsdict[d["ts"]] = {"lcor" : d["lcor"], "rcor" : d["rcor"], "l1sd" : d["l1sd"], "l2sd" : d["l2sd"], "r1sd" : d["r1sd"], "r2sd" : d["r2sd"]}
+        fndict[d["fn"]] = {
+            "ts":d["ts"], "lcor":d["lcor"], "rcor":d["rcor"], "l1sd":d["l1sd"], "l2sd":d["l2sd"], "r1sd":d["r1sd"], "r2sd":d["r2sd"]
+        }
+    return fndict
 
-    for blinks, blinkLabelPrefix, yval in [(lBlinks, "l", 0.998), (rBlinks, "r", 0.997)]:
-        for blink in blinks:
-            for eventName, eventNameTranslation in [("start", blinkLabelPrefix+"bs"), ("end", blinkLabelPrefix+"be")]:
-                tsdict[blink[eventName]][eventNameTranslation] = yval
+def addDetectedBlinks(fndict):
+    for blink in lBlinks:
+        for frame, timing in [("fs", "lbs"), ("fe", "lbe")]:
+            fndict[blink[frame]][timing] = 0.998
 
+    for blink in rBlinks:
+        for frame, timing in [("fs", "rbs"), ("fe", "rbe")]:
+            fndict[blink[frame]][timing] = 0.997
+    return
+
+def writeBlinkToFndict(fndict, curBlinkId, curBlinkStart, curBlinkEnd, curBlinkLC, curBlinkRC):
+    try:
+        fndict[int(curBlinkStart)]["anots"] = 0.999
+        fndict[int(curBlinkEnd)]["anote"] = 0.999
+    except KeyError:
+        print "skipping %s %s %s" % (curBlinkId, curBlinkStart, curBlinkEnd)
+
+
+def parseAnnotations(f, fndict):
+    curBlinkId = ""
+    curBlinkStart = ""
+    curBlinkEnd = ""
+    curBlinkLC = []
+    curBlinkRC = []
+
+    content = False
+    for fline in f:
+        fline = fline.strip()
+        if not content:
+            if fline == "#start":
+                content = True
+            continue
+        else:
+            if fline == "#end":
+                break
+
+        # 0 frameCounter 1 blinkID 2 nonFrontalFace 3 leftFullyClosed 4 leftNonFrontal 5 rightFullyClosed 6 rightNonFrontal
+        line = fline.split(":")
+        if line[1] != curBlinkId:
+            if line[1] == "-1":
+                if curBlinkId == "":
+                    continue
+                else:
+                    writeBlinkToFndict(fndict, curBlinkId, curBlinkStart, curBlinkEnd, curBlinkLC, curBlinkRC)
+                    curBlinkId    = ""
+                    curBlinkStart = ""
+                    curBlinkEnd   = ""
+            else:
+                if curBlinkId == "":
+                    curBlinkId = line[1]
+                    curBlinkStart = line[0]
+                else:
+                    writeBlinkToFndict(fndict, curBlinkId, curBlinkStart, curBlinkEnd, curBlinkLC, curBlinkRC)
+                    curBlinkId    = line[1]
+                    curBlinkStart = line[0]
+                    curBlinkEnd   = ""
+        # still the same, can also parse FullyClosed
+        curBlinkEnd = line[0]
+
+    if curBlinkId != "":
+        writeBlinkToFndict(fndict, curBlinkId, curBlinkStart, curBlinkEnd, curBlinkLC, curBlinkRC)
+
+    return
+
+def generateTCSV():
+    fndict = buildFndict()
+    addDetectedBlinks(fndict)
+    f = file(vidPrefix+videoAnnot)
+    parseAnnotations(f, fndict)
+
+
+    """
     annotationsFile =  "annotations/"+videoName
     if os.path.isfile(annotationsFile):
         annotated = file(annotationsFile).read().strip()
@@ -38,33 +113,78 @@ def generateTCSV():
                 tsdict[afinished]["anote"] = 0.999
             except:
                 print "No key %s %s %s" % (astart, aclosed, afinished)
+    """
+    # sort by frame num
+    fnl = sorted(fndict.items(), key=lambda x:float(x[0]))
+    return fndict, fnl
 
-    tsl = sorted(tsdict.items(), key=lambda x:float(x[0]))
-    return tsdict, tsl
-
-def writeTCSV(tsl):
+def writeTCSV(fnl):
     pref = path+"outputs/"
     f = file(pref+"out.csv","wb")
-    for e in tsl:
-        ws = e[0]
-        d = e[1]
 
-        for t in ["lcor", "rcor", "l1sd", "l2sd", "r1sd", "r2sd"]:
-            ws += "\t%.6f" % d[t]
+    for frameNum, data in fnl:
+        line = ""
+        line += str(frameNum)
+
+        for t in ["lcor", "rcor", "l1sd", "l2sd", "r1sd", "r2sd"]: # "ts", 
+            line += "\t%.6f" % data[t]
 
         for t in ["lbs", "lbe", "rbs", "rbe"]:
-            ws += "\t"
-            if d.has_key(t):
-                ws += "%.3f" % d[t]
+            line += "\t"
+            if data.has_key(t):
+                line += "%.3f" % data[t]
 
         for t in ["anots", "anotc", "anote"]:
-            ws += "\t"
-            if d.has_key(t):
-                ws += "%.3f" % d[t]
+            line += "\t"
+            if data.has_key(t):
+                line += "%.3f" % data[t]
 
-        ws += "\n"
-        f.write(ws)
+        line += "\n"
+        f.write(line)
     f.close()
+
+def processLogLine(output):
+    global lBlinks, rBlinks, tCors
+    if output.startswith("debug_blinks_d1:"):
+        corsInfo = output.split(" ")
+        print repr(corsInfo)
+        if (corsInfo[1:] == ['blinkMeasureSize', 'is', 'zero']
+            or corsInfo[3:7] == ['shortBmSize', 'is', 'less', 'than']
+            or corsInfo[3:7] == ['fps', 'of', 'the', 'first']
+            or corsInfo[3:-1] == ['shortBmSize', 'is', 'big', 'enough']
+            or corsInfo[1:3] == ['updated', 'maxFramesShortList']
+            ):
+            return False
+        fn   = int(corsInfo[corsInfo.index("lastF")+1])
+        ts   = float(corsInfo[corsInfo.index("T")+1])
+        lcor = float(corsInfo[corsInfo.index("La")+1])
+        rcor = float(corsInfo[corsInfo.index("Ra")+1])
+        l1sd = float(corsInfo[corsInfo.index("lSD12")+2])
+        l2sd = float(corsInfo[corsInfo.index("lSD12")+3])
+        r1sd = float(corsInfo[corsInfo.index("rSD12")+2])
+        r2sd = float(corsInfo[corsInfo.index("rSD12")+3])
+        tCors.append({"fn": fn, "ts" : ts, "lcor" : lcor, "rcor" : rcor, "l1sd" : l1sd, "l2sd" : l2sd, "r1sd" : r1sd, "r2sd" : r2sd})
+    elif output.startswith("debug_blinks_d4:"):
+        blinkInfo = output.split(" ")
+        if blinkInfo[1] == "adding_lBlinkChunks":
+            lst = lBlinks
+        elif blinkInfo[1] == "adding_rBlinkChunks":
+            lst = rBlinks
+
+        fs = int(blinkInfo[blinkInfo.index("fs")+1])
+        fe = int(blinkInfo[blinkInfo.index("fe")+1])
+        start     = float(blinkInfo[blinkInfo.index("start")+1])
+        end       = float(blinkInfo[blinkInfo.index("end")+1])
+        duration  = float(blinkInfo[blinkInfo.index("duration")+1])
+        if start > 1000000000:
+            start /= 1000.
+            end /= 1000.
+        #start = datetime.datetime.fromtimestamp(start)
+        blinkInfoDict = {"fs":fs, "fe":fe, "start":start, "end":end, "duration":duration}
+        lst.append(blinkInfoDict)
+    elif output.startswith("exiting"):
+        return True
+    return False
 
 # parsing logs
 def initListenLog():
@@ -77,9 +197,8 @@ def initListenLog():
         stdout  = subprocess.PIPE,
         bufsize = 0,
     )
-
 def listenLog():
-    global stopListenLogStopped, lBlinks, rBlinks, tCors
+    global stopListenLogStopped
 
     poll_obj = select.poll()
     poll_obj.register(proc.stdout, select.POLLIN)
@@ -87,46 +206,14 @@ def listenLog():
         poll_result = poll_obj.poll(0)
         if poll_result:
             output = proc.stdout.readline().strip()
-
-            if output.startswith("debug_blinks_d1:"):
-                corsInfo = output.split(" ")
-                print repr(corsInfo)
-                if (corsInfo[1:] == ['blinkMeasureSize', 'is', 'zero'] or
-                    corsInfo[1:-1] == ['shortBmSize', 'is', 'less', 'than', 'X'] or
-                    corsInfo[1:-1] == ['shortBmSize', 'is']):
-                    continue
-                ts   = "%.2f" % float(corsInfo[corsInfo.index("lastT")+1])
-                lcor = float(corsInfo[corsInfo.index("La")+1])
-                rcor = float(corsInfo[corsInfo.index("Ra")+1])
-                l1sd = float(corsInfo[corsInfo.index("lSD12")+2])
-                l2sd = float(corsInfo[corsInfo.index("lSD12")+3])
-                r1sd = float(corsInfo[corsInfo.index("rSD12")+2])
-                r2sd = float(corsInfo[corsInfo.index("rSD12")+3])
-                tCors.append({"ts" : ts, "lcor" : lcor, "rcor" : rcor, "l1sd" : l1sd, "l2sd" : l2sd, "r1sd" : r1sd, "r2sd" : r2sd})
-            elif output.startswith("debug_blinks_d4:"):
-                blinkInfo = output.split(" ")
-                start     = float(blinkInfo[blinkInfo.index("start")+1])
-                end       = float(blinkInfo[blinkInfo.index("end")+1])
-                duration  = float(blinkInfo[blinkInfo.index("duration")+1])
-                if start > 1000000000:
-                    start /= 1000.
-                    end /= 1000.
-                start = "%.2f" % start
-                end   = "%.2f" % end
-                #start = datetime.datetime.fromtimestamp(start)
-
-                blinkInfoDict = {"start":start, "end":end, "duration":duration}
-
-                if blinkInfo[1] == "adding_lBlinkChunks":
-                    lBlinks.append(blinkInfoDict)
-                elif blinkInfo[1] == "adding_rBlinkChunks":
-                    rBlinks.append(blinkInfoDict)
-
-                print "adding:", blinkInfo[1], blinkInfo
-            elif output.startswith("exiting"):
+            try:
+                if processLogLine(output):
+                    stopListenLogStopped = True
+                    break
+            except:
+                print "crash", output
                 stopListenLogStopped = True
                 break
-
             continue
 
         time.sleep(0.1)
@@ -143,8 +230,7 @@ def terminateListenLog():
 def initRunVideo():
     global vid
     vid = subprocess.Popen(
-        ['bash', 'compileDesktop', 'test'
-        ],
+        ['make', 'dt'],
         cwd     = '/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/optical-flow',
         stdin   = subprocess.PIPE,
         stdout  = subprocess.PIPE,
--- testmain.py
+++ testmain.py
@@ -4,7 +4,7 @@ import main
 class Test(unittest.TestCase):
     def setUp(self):
         pass
-    def testGenerateCSV(self):
+    def _testGenerateTCSV(self):
         main.lBlinks = [
             {'duration': 266.666667, 'start': 2400.0,  'end': 2666.666667},#l1
             {'duration': 133.333333, 'start': 2733.33, 'end': 2866.666667},#l2
@@ -20,7 +20,7 @@ class Test(unittest.TestCase):
             {'duration': 66.666667,  'start': 9966.67, 'end': 10033.333333}#r5
         ]
 
-        tsdict, tsl = main.generateCSV()
+        tsdict, tsl = main.generateTCSV()
         self.assertEquals(17, len(tsl))
         self.assertEquals((2266.67, {'rbs': 1}), tsl[0])#r1
         self.assertEquals((2400.0, {'lbs': 1}), tsl[1])#l1
@@ -39,6 +39,103 @@ class Test(unittest.TestCase):
         self.assertEquals((9966.67, {'rbs': 1}), tsl[14])#r5
         self.assertEquals((10033.333333, {'rbe': 1}), tsl[15])#r5e
         self.assertEquals((10100.0, {'lbe': 1}), tsl[16])#l5e
+    def testProcessLogLine(self):
+        main.lBlinks = []
+        main.rBlinks = []
+        main.tCors   = []
+        r = main.processLogLine("debug_blinks_d4: adding_lBlinkChunks fs 169 fe 173 start 5666.67 end 5800.000000 duration 133.333333")
+
+    def testParseAnnot1(self):
+        tmpContent = ""
+        tmpContent += "#eye-blink annotation file version 1.1\n"
+        tmpContent += "#author:  fogelton\n"
+        tmpContent += "#glasses: NO\n"
+        tmpContent += "#challenging: NO\n"
+        tmpContent += "#message start\n"
+        tmpContent += "\n"
+        tmpContent += "\n"
+        tmpContent += "#message end\n"
+        tmpContent += "#start\n"
+        tmpContent += "0:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "1:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "2:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "3:1:X:X:X:X:X:251:221:247:290:297:288:348:282:409:270:459:267\n"
+        tmpContent += "4:1:X:C:X:C:X:251:221:247:290:297:288:348:282:409:270:459:267\n"
+        tmpContent += "5:1:X:X:X:X:X:251:221:247:290:297:288:348:282:409:270:459:267\n"
+        tmpContent += "6:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "7:2:X:X:X:X:X:251:221:247:290:297:288:348:282:409:270:459:267\n"
+        tmpContent += "8:2:X:C:X:C:X:251:221:247:290:297:288:348:282:409:270:459:267\n"
+        tmpContent += "9:2:X:X:X:X:X:251:221:247:290:297:288:348:282:409:270:459:267\n"
+        tmpContent += "10:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += ""
+        tmpContent += "#end"
+        file("/tmp/.testParseAnnot", "wb").write(tmpContent)
+        f = file("/tmp/.testParseAnnot", "rb")
+
+        fndict = {}
+        for i in xrange(100):
+            fndict[i] = {}
+
+        main.parseAnnotations(f, fndict)
+
+        self.assertEquals({'anots': 0.999}, fndict[3])
+        self.assertEquals({'anote': 0.999}, fndict[5])
+        self.assertEquals({'anots': 0.999}, fndict[7])
+        self.assertEquals({'anote': 0.999}, fndict[9])
+
+    def testParseAnnot2(self):
+        tmpContent = ""
+        tmpContent += "#start\n"
+        tmpContent += "0:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "1:1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "2:-1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "#end"
+        file("/tmp/.testParseAnnot", "wb").write(tmpContent)
+        f = file("/tmp/.testParseAnnot", "rb")
+
+        fndict = {}
+        for i in xrange(100):
+            fndict[i] = {}
+
+        main.parseAnnotations(f, fndict)
+
+        self.assertEquals({'anots': 0.999, 'anote': 0.999}, fndict[1])
+
+    def testParseAnnot3(self):
+        tmpContent = ""
+        tmpContent += "#start\n"
+        tmpContent += "0:1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "#end"
+        file("/tmp/.testParseAnnot", "wb").write(tmpContent)
+        f = file("/tmp/.testParseAnnot", "rb")
+
+        fndict = {}
+        for i in xrange(100):
+            fndict[i] = {}
+
+        main.parseAnnotations(f, fndict)
+
+        self.assertEquals({'anots': 0.999, 'anote': 0.999}, fndict[0])
+
+    def testParseAnnot4(self):
+        tmpContent = ""
+        tmpContent += "#start\n"
+        tmpContent += "0:1:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "1:2:X:X:X:X:X:260:194:251:290:299:270:351:259:416:246:466:243\n"
+        tmpContent += "#end"
+        file("/tmp/.testParseAnnot", "wb").write(tmpContent)
+        f = file("/tmp/.testParseAnnot", "rb")
+
+        fndict = {}
+        for i in xrange(100):
+            fndict[i] = {}
+
+        main.parseAnnotations(f, fndict)
+
+        self.assertEquals({'anots': 0.999, 'anote': 0.999}, fndict[0])
+        self.assertEquals({'anots': 0.999, 'anote': 0.999}, fndict[1])
+
+
 
 if __name__ =="__main__":
     unittest.main()
