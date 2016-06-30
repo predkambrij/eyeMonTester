6b14e0f888de1933cf86912f35af51d14fdc36cd
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-06-30 12:23:35 +0200

    upd minimizing global vars

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import os
 import processVideo
 
 
-#videoName = "o44" # doma
-#videoName = "o89" # knjiznica 40s
-#videoName = "o90" # premikal glavo, zadej luc
-vidPrefix = "/home/developer/other/posnetki/"
-
-
-vidNum = 1
-
-if vidNum == 0:
-    videoName = ""
-elif vidNum == 1:
-    vidPrefix += "sk/eyeblink8/1/" # punca od dalec
-    videoName = "26122013_223310_cam.avi"
-elif vidNum == 2:
-    vidPrefix += "sk/NightOfResearchers15/test/14/" # full partial
-    videoName = "26092014_211047_cam.avi"
-elif vidNum == 3:
-    videoName = "talking.avi"
-
-videoAnnot = os.path.splitext(videoName)[0]+".tag"
-
 def main():
+    outputs = "vidProcOutputs"
+
+    #videoName = "o44" # doma
+    #videoName = "o89" # knjiznica 40s
+    #videoName = "o90" # premikal glavo, zadej luc
+    vidPrefix = "/home/developer/other/posnetki/"
+
+
+    vidNum = 1
+
+    if vidNum == 0:
+        videoName = ""
+    elif vidNum == 1:
+        vidPrefix += "sk/eyeblink8/1/" # punca od dalec
+        videoName = "26122013_223310_cam.avi"
+    elif vidNum == 2:
+        vidPrefix += "sk/NightOfResearchers15/test/14/" # full partial
+        videoName = "26092014_211047_cam.avi"
+    elif vidNum == 3:
+        videoName = "talking.avi"
+
+    videoAnnot = os.path.splitext(videoName)[0]+".tag"
+
     processVideo.processVideo(vidNum, vidPrefix, videoAnnot)
     return
 
