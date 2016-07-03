c2d07fc0656068112bd3992b63d2eeb851ca57f0
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-07-04 01:15:45 +0200

    add other interesting videos

--- main.py
+++ main.py
@@ -17,21 +17,132 @@ def prepareVideosList(cfg):
         ("", vidPrefix+"sk/eyeblink8/2/26122013_224532_cam.avi"),
         ("", vidPrefix+"sk/eyeblink8/4/26122013_230654_cam.avi"),
     ]
+    nightOfResearchers30 = [
+        ("", vidPrefix+"sk/NightOfResearchers30/test/1/26092014_161749_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/10/26092014_173024_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/11/26092014_173630_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/12/26092014_175943_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/13/26092014_181011_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/14/26092014_182857_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/15/26092014_111716_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/16/26092014_183734_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/17/26092014_121151_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/18/26092014_184632_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/19/26092014_185952_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/2/26092014_150500_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/20/26092014_134306_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/21/26092014_191620_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/22/26092014_194520_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/23/26092014_195901_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/24/26092014_200714_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/25/26092014_201130_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/26/26092014_203937_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/27/26092014_204339_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/28/26092014_141144_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/29/26092014_210427_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/3/26092014_160051_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/30/26092014_125304_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/31/26092014_133722_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/32/26092014_130857_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/33/26092014_134925_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/34/26092014_142548_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/35/26092014_161141_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/4/26092014_163504_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/5/26092014_164703_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/6/26092014_164731_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/7/26092014_105214_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/8/26092014_141058_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/test/9/26092014_172120_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/1/26092014_113824_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/10/26092014_195612_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/11/26092014_144314_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/12/26092014_130620_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/13/26092014_204644_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/14/26092014_133535_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/15/26092014_134156_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/16/26092014_181133_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/17/26092014_160056_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/2/26092014_175010_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/3/26092014_133821_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/4/26092014_185440_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/5/26092014_191449_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/6/26092014_121842_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/7/26092014_192316_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/8/26092014_192746_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/train/9/26092014_195001_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/1/26092014_133154_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/10/26092014_122529_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/11/26092014_200204_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/12/26092014_122600_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/13/26092014_201335_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/14/26092014_203825_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/15/26092014_204844_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/16/26092014_211251_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/17/26092014_130017_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/2/26092014_162458_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/3/26092014_161025_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/4/26092014_171745_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/5/26092014_174355_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/6/26092014_181244_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/7/26092014_184450_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/8/26092014_184654_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers30/trainval/val/9/26092014_190156_cam.avi"),
+    ]
+    nightOfResearchers15 = [
+        ("", vidPrefix+"sk/NightOfResearchers15/test/1/26092014_181912_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/10/26092014_203647_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/11/26092014_204021_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/12/26092014_205740_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/13/26092014_210519_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/14/26092014_211047_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/15/26092014_175102_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/16/26092014_175705_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/17/26092014_180329_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/18/26092014_180951_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/19/26092014_211637_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/2/26092014_183209_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/3/26092014_185953_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/4/26092014_191626_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/5/26092014_192313_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/6/26092014_200033_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/7/26092014_200537_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/8/26092014_201335_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/test/9/26092014_202134_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/1/26092014_172946_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/10/26092014_202954_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/2/26092014_181537_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/3/26092014_182216_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/4/26092014_182617_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/5/26092014_183818_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/6/26092014_185756_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/7/26092014_173827_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/8/26092014_190804_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/train/9/26092014_192934_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/1/26092014_184528_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/2/26092014_185044_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/3/26092014_190424_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/4/26092014_194530_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/5/26092014_195100_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/6/26092014_174618_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/7/26092014_203130_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/8/26092014_205352_cam.avi"),
+        ("", vidPrefix+"sk/NightOfResearchers15/trainval/val/9/26092014_175359_cam.avi"),
+    ]
     others = [
-        ("full partial", vidPrefix+"sk/NightOfResearchers15/test/14/26092014_211047_cam.avi"),
+        #("full partial", vidPrefix+"sk/NightOfResearchers15/test/14/26092014_211047_cam.avi"),
         ("talking", vidPrefix+"talking.avi"),
     ]
-    videos = others+eyeblink8HandInTheFirstFrames+eyeblink8
+    videos = nightOfResearchers15+nightOfResearchers30+eyeblink8HandInTheFirstFrames+eyeblink8+others
     videoRange = range(len(videos))
     #videoRange = [4] # punca od dalec
-    videoRange = [8] # dobra slika
+    #videoRange = [8] # dobra slika
     #videoRange = [0]
     return videos, videoRange
 
 def getConfigs():
     #method = "blackpixels"
-    method = "templ"
-    #method = "farneback"
+    #method = "templ"
+    method = "farneback"
     return {
         "excel_export": False,
         "coverage":     False,
@@ -43,9 +154,8 @@ def getConfigs():
             "codeDirectory":"/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/test_runner",
 
             #"outputsPref":"/vidProcOutputs",
-            #"outputsPref":"/vidProcOutputs/verTest",
-            #"outputsPref":"/vidProcOutputs/ver1", # farne
-            "outputsPref":"/vidProcOutputs/ver1"+method,
+            #"outputsPref":"/vidProcOutputs/ver1",
+            "outputsPref":"/vidProcOutputs/ver2"+method,
         }
     }
 
